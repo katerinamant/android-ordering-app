3d063684b4d95b458c43654ffe97e04d245d8ebb
Bill Zafeiris <bzafiris@gmail.com>
2023-12-02 12:08:48 +0200

    Added a simple unit test

---
+++ app/src/test/java/com/example/softcafeengineer/domain/UserTest.java
@@ -0,0 +1,18 @@
+package com.example.softcafeengineer.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void setPassword() {
+        assertEquals(10l, 10);
+    }
+}
