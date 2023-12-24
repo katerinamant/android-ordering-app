9a0f1694fb53e4ac8ad6baaf9425872fa43e039c
Sofia Sotiriou <szsotiriou@gmail.com>
2023-12-24 11:41:42 +0200

    Additions to SignUp Classes and layout

--- app/src/main/java/com/example/softcafeengineer/view/Manager/SignUp/ManagerSignUpActivity.java
+++ app/src/main/java/com/example/softcafeengineer/view/Manager/SignUp/ManagerSignUpActivity.java
@@ -2,9 +2,11 @@ package com.example.softcafeengineer.view.Manager.SignUp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Toast;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.view.View;
 
 import com.example.softcafeengineer.R;
 import com.example.softcafeengineer.memorydao.ManagerDAOMemory ;
@@ -12,9 +14,11 @@ import com.example.softcafeengineer.memorydao.ManagerDAOMemory ;
 public class ManagerSignUpActivity extends AppCompatActivity implements ManagerSignUpView
 {
     private ManagerSignUpView viewModel;
-    private Button signUp;
+    private Button continueButton;
     private boolean signUp_enabled;
-    private EditText username, password, address, phoneNumber, SSN, brand;
+    private EditText usernameField, passwordField, addressField, phoneNumberField, ssnField, brandField;
+    private String username, password, address, brand;
+    private int phoneNumber, ssn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +27,26 @@ public class ManagerSignUpActivity extends AppCompatActivity implements ManagerS
 
         final ManagerSignUpPresenter presenter = new ManagerSignUpPresenter(this, new ManagerDAOMemory());
 
+        signUp_enabled = false;
+        usernameField = findViewById(R.id.edit_txt_mngr_username_signup);
+        passwordField = findViewById(R.id.edit_txt_mngr_password_signup);
+//        addressField = findViewById(R.id.edit_txt_mngr_address_signup);
+//        phoneNumberField = findViewById(R.id.edit_txt_mngr_phoneNumber_signup);
+//        ssnField = findViewById(R.id.edit_txt_mngr_ssn_signup);
+//        brandField = findViewById(R.id.edit_txt_mngr_brand_signup);
+        continueButton = findViewById(R.id.edit_txt_mngr_continue_signup);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {presenter.onContinue(signUp_enabled, username, password);};
+        });
+
+
+    }
+
+    @Override
+    public void showToast(String msg)
+    {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/softcafeengineer/view/Manager/SignUp/ManagerSignUpPresenter.java
+++ app/src/main/java/com/example/softcafeengineer/view/Manager/SignUp/ManagerSignUpPresenter.java
@@ -1,6 +1,8 @@
 package com.example.softcafeengineer.view.Manager.SignUp;
 
 import com.example.softcafeengineer.dao.ManagerDAO;
+import com.example.softcafeengineer.domain.User;
+
 public class ManagerSignUpPresenter {
     private ManagerSignUpView view;
     private ManagerDAO users;
@@ -9,7 +11,21 @@ public class ManagerSignUpPresenter {
     {
         this.view = view;
         this.users = users;
+    }
 
-
+    public void onContinue(boolean button_enabled, String username, String password)
+    {
+        if (!button_enabled)
+        {
+            view.showToast("Please fill the required fields");
+        }
+        else
+        {
+            User newUser = new User(username, password);
+            if (!users.save(newUser))
+            {
+              view.showToast("This username is already in use. Please choose a different one");
+            }
+        }
     }
 }
