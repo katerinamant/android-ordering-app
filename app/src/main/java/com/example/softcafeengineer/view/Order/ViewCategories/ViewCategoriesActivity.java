e7cd2ff6e279336634645dc2818a7c823d7326c1
katerinamant <katerinamantaraki@gmail.com>
2024-01-05 22:23:42 +0200

    set up Order.ViewCategories

---
+++ app/src/main/java/com/example/softcafeengineer/view/Order/ViewCategories/ViewCategoriesActivity.java
@@ -0,0 +1,16 @@
+package com.example.softcafeengineer.view.Order.ViewCategories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.softcafeengineer.R;
+
+public class ViewCategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_categories);
+    }
+}
