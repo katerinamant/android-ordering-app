0d41d04a8c7ee66725b01c032b3084b0de2a3d7e
Rea Sklika <reoyla@gmail.com>
2024-01-06 17:52:29 +0200

    add tests for ManagerEmployeesActivity

--- app/src/test/java/com/example/softcafeengineer/view/Manager/ManageEmployees/ManageEmployeesViewStub.java
+++ app/src/test/java/com/example/softcafeengineer/view/Manager/ManageEmployees/ManageEmployeesViewStub.java
@@ -1,2 +1,63 @@
-package com.example.softcafeengineer.view.Manager.ManageEmployees;public class ManageEmployeesViewStub {
+package com.example.softcafeengineer.view.Manager.ManageEmployees;
+
+public class ManageEmployeesViewStub implements ManageEmployeesView
+{
+    private String username, password, errorTitle, errorMessage, toastMessage;
+    private ManageEmployeesPresenter presenter;
+
+    public ManageEmployeesViewStub(){
+        username = password = errorTitle = errorMessage = toastMessage = "";
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getErrorTitle(){
+        return errorTitle;
+    }
+    public String getErrorMessage(){
+        return errorMessage;
+    }
+    public String getToastMessage(){
+        return toastMessage;
+    }
+    public ManageEmployeesPresenter getPresenter(){
+        return presenter;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public void setPresenter(ManageEmployeesPresenter presenter){
+        this.presenter = presenter;
+    }
+    @Override
+    public void successfulNewEmployee() {
+
+    }
+
+    @Override
+    public void successfulEdit() {
+
+    }
+
+    @Override
+    public void successfulDelete() {
+
+    }
+
+    @Override
+    public void showError(String title, String msg) {
+        errorTitle = title;
+        errorMessage = msg;
+    }
+
+    @Override
+    public void showToast(String msg) {
+        toastMessage = msg;
+    }
 }
