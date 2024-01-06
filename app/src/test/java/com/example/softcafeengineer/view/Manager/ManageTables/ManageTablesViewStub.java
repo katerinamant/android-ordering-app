f211aa192776280c06885ee3986a82a1a8db2daa
Rea Sklika <reoyla@gmail.com>
2024-01-06 17:43:35 +0200

    add tests for ManageTablesActivity

--- app/src/test/java/com/example/softcafeengineer/view/Manager/ManageTables/ManageTablesViewStub.java
+++ app/src/test/java/com/example/softcafeengineer/view/Manager/ManageTables/ManageTablesViewStub.java
@@ -1,2 +1,66 @@
-package com.example.softcafeengineer.view.Manager.ManageTables;public class ManageTablesViewStub {
+package com.example.softcafeengineer.view.Manager.ManageTables;
+
+import com.example.softcafeengineer.view.Manager.ManageEmployees.ManageEmployeesPresenter;
+
+public class ManageTablesViewStub implements ManageTablesView
+{
+    private String QRCode, tableID, errorTitle, errorMessage, toastMessage;
+
+    private ManageTablesPresenter presenter;
+    public ManageTablesViewStub(){
+        QRCode = tableID = "";
+    }
+    public String getQRCode(){
+        return QRCode;
+    }
+    public String getTableID(){
+        return tableID;
+    }
+    public String getErrorTitle(){
+        return errorTitle;
+    }
+    public String getErrorMessage(){
+        return errorMessage;
+    }
+    public String getToastMessage(){
+        return toastMessage;
+    }
+    public ManageTablesPresenter getPresenter(){
+        return presenter;
+    }
+    public void setQRCode(String QRCode){
+        this.QRCode = QRCode;
+    }
+    public void setTableID(String tableID){
+        this.tableID = tableID;
+    }
+    public void setPresenter(ManageTablesPresenter presenter){
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void successfulNewTable() {
+
+    }
+
+    @Override
+    public void successfulEdit() {
+
+    }
+
+    @Override
+    public void successfulDelete() {
+
+    }
+
+    @Override
+    public void showError(String title, String msg) {
+        errorTitle = title;
+        errorMessage = msg;
+    }
+
+    @Override
+    public void showToast(String msg) {
+        toastMessage = msg;
+    }
 }
