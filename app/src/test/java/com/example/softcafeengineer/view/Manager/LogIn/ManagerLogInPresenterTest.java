035513d0b34bed26e8f38eaa477bf27c56e7dfa4
Rea Sklika <reoyla@gmail.com>
2024-01-06 17:55:51 +0200

    add tests for ManagerLogInActivity

--- app/src/test/java/com/example/softcafeengineer/view/Manager/LogIn/ManagerLogInPresenterTest.java
+++ app/src/test/java/com/example/softcafeengineer/view/Manager/LogIn/ManagerLogInPresenterTest.java
@@ -1,2 +1,47 @@
-package com.example.softcafeengineer.view.Manager.LogIn;public class LogInPresenterTest {
+package com.example.softcafeengineer.view.Manager.LogIn;
+
+import com.example.softcafeengineer.dao.ManagerDAO;
+import com.example.softcafeengineer.domain.User;
+import com.example.softcafeengineer.memorydao.ManagerDAOMemory;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ManagerLogInPresenterTest {
+    ManagerLogInPresenter presenter;
+    ManagerLogInViewStub view;
+    ManagerDAO managerDAO;
+    User user;
+
+    @Before
+    public void setUp(){
+        view = new ManagerLogInViewStub();
+        managerDAO = new ManagerDAOMemory();
+
+        presenter = new ManagerLogInPresenter(view, managerDAO);
+        user = new User("login_username", "password");
+        managerDAO.save(user);
+    }
+
+    @Test
+    public void testWrongPassword(){
+        presenter.onLogin(true, "login_username","wrong_password");
+        Assert.assertEquals(view.getErrorTitle(),"Log In unsuccessful.");
+        Assert.assertEquals(view.getErrorMessage(),"The credentials provided were invalid. Try again.");
+    }
+
+    @Test
+    public void testWrongUsername(){
+        presenter.onLogin(true, "wrong_username","password");
+        Assert.assertEquals(view.getErrorTitle(),"Log In unsuccessful.");
+        Assert.assertEquals(view.getErrorMessage(),"The credentials provided were invalid. Try again.");
+    }
+
+    @Test
+    public void testDisabledButton(){
+        presenter.onLogin(false, "","");
+        Assert.assertEquals(view.getToastMessage(),"Please fill the required fields.");
+    }
+
 }
--- app/src/test/java/com/example/softcafeengineer/view/Manager/LogIn/ManagerLogInViewStub.java
+++ app/src/test/java/com/example/softcafeengineer/view/Manager/LogIn/ManagerLogInViewStub.java
@@ -1,2 +1,57 @@
-package com.example.softcafeengineer.view.Manager.LogIn;public class LogInViewStub {
+package com.example.softcafeengineer.view.Manager.LogIn;
+
+import com.example.softcafeengineer.domain.Cafeteria;
+import com.example.softcafeengineer.view.Manager.ManageEmployees.ManageEmployeesPresenter;
+import com.example.softcafeengineer.view.Manager.ManageEmployees.ManageEmployeesViewStub;
+
+public class ManagerLogInViewStub implements ManagerLogInView
+{
+    private String username, password, errorTitle, errorMessage, toastMessage;
+    private ManagerLogInPresenter presenter;
+
+    public ManagerLogInViewStub(){
+        username = password = errorTitle = errorMessage = toastMessage = "";
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getErrorTitle(){
+        return errorTitle;
+    }
+    public String getErrorMessage(){
+        return errorMessage;
+    }
+    public String getToastMessage(){
+        return toastMessage;
+    }
+    public ManagerLogInPresenter getPresenter(){
+        return presenter;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public void setPresenter(ManagerLogInPresenter presenter){
+        this.presenter = presenter;
+    }
+    @Override
+    public void successfulLogIn(Cafeteria cafe) {
+
+    }
+
+    @Override
+    public void showError(String title, String msg) {
+        errorTitle = title;
+        errorMessage = msg;
+    }
+
+    @Override
+    public void showToast(String msg) {
+        toastMessage = msg;
+    }
 }
